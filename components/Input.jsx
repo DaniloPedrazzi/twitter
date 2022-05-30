2564e9d8892197e9b27d947e4fa644bea98eb9fa
Danilo <danilo.justino1@gmail.com>
2022-05-30 15:33:03 -0300

    Feed + Auth

--- components/Input.jsx
+++ components/Input.jsx
@@ -1,8 +1,6 @@
 /* Mensagem pro eu do futuro:
 Desculpa pelo código complexo, um dia vc vai entender */
 
-import { CalendarIcon, ChartBarIcon, EmojiHappyIcon, PhotographIcon } from "@heroicons/react/outline";
-import { XIcon } from "@heroicons/react/solid";
 import { useState, useRef } from "react";
 import { db, storage } from "../firebase";
 import {
@@ -13,13 +11,21 @@ import {
   updateDoc,
 } from "@firebase/firestore";
 import { getDownloadURL, ref, uploadString } from "@firebase/storage";
-
-
+import { useSession } from "next-auth/react";
+import { XIcon } from "@heroicons/react/solid";
+import {
+  CalendarIcon,
+  ChartBarIcon,
+  EmojiHappyIcon,
+  PhotographIcon,
+} from "@heroicons/react/outline";
 
 function Input() {
+  const { data: session } = useSession();
+
   const [input, setInput] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const filePickerRef = useRef(null);
 
   const sendPost = async () => {
@@ -30,13 +36,13 @@ function Input() {
 
     //manda o post pra db
     const docRef = await addDoc(collection(db, "posts"), {
-      // id: session.user.uid,
-      // username: session.user.name,
-      // userImg: session.user.image,
-      // tag: session.user.tag,
+      id: session.user.uid,
+      username: session.user.name,
+      userImg: session.user.image,
+      tag: session.user.tag,
       text: input,
       timestamp: serverTimestamp(),
-    })
+    });
 
     //cria e manda a url da imagem pro db
     const imageRef = ref(storage, `posts/${docRef.id}/image`);
@@ -52,7 +58,7 @@ function Input() {
     setInput("");
     setSelectedFile(null);
     setLoading(false);
-  }
+  };
 
   //mostra e seta a imagem no input
   const addImageToPost = (e) => {
@@ -63,20 +69,25 @@ function Input() {
 
     reader.onload = (readerEvent) => {
       setSelectedFile(readerEvent.target.result);
-    }
-  }
+    };
+  };
 
   return (
-    <div className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll 
+    <div
+      className={`border-b border-gray-700 p-3 flex space-x-3 overflow-y-scroll scrollbar-hide 
       ${loading && "opacity-60"}`}
     >
-      <img src="https://pbs.twimg.com/profile_images/1405731311914913794/c-40xdgh_400x400.jpg" alt=""
-        className="h-11 w-11 rounded-full cursor-pointer" />
+      <img
+        src={session.user.image}
+        alt=""
+        className="h-11 w-11 rounded-full cursor-pointer"
+      />
       <div className="w-full divide-y divide-gray-700">
-
         {/* Text Area */}
         <div className={`${selectedFile && "pb-7"} ${input && "space-y-2.5"}`}>
-          <textarea value={input} onChange={(e) => setInput(e.target.value)}
+          <textarea
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
             className="bg-transparent outline-none text-[#d9d9d9] text-lg 
             placeholder-gray-500 tracking-wide w-full min-h-[50px]"
             placeholder="What's happening?"
@@ -86,24 +97,40 @@ function Input() {
           {/* Image */}
           {selectedFile && ( //if selectedFile == true
             <div className="relative">
-              <div onClick={() => setSelectedFile(null)}
+              <div
+                onClick={() => setSelectedFile(null)}
                 className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 
-                rounded-full flex items-center justify-center top-1 left-1 cursor-pointer">
+                rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
+              >
                 <XIcon className="text-white h-5" />
               </div>
-              <img src={selectedFile} alt="" className="rounded-2xl max-h-80 object-contain" />
+              <img
+                src={selectedFile}
+                alt=""
+                className="rounded-2xl max-h-80 object-contain"
+              />
             </div>
           )}
         </div>
 
         {!loading && (
           <div className="flex items-center justify-between pt-2.5">
-
             {/* Icons */}
             <div className="flex items-center">
-              <div className="icon" onClick={() => filePickerRef.current.click() /* clica na div e ativa o input */}>
+              <div
+                className="icon"
+                onClick={
+                  () =>
+                    filePickerRef.current.click() /* clica na div e ativa o input */
+                }
+              >
                 <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
-                <input type="file" onChange={addImageToPost} ref={filePickerRef} hidden />
+                <input
+                  type="file"
+                  onChange={addImageToPost}
+                  ref={filePickerRef}
+                  hidden
+                />
               </div>
               <div className="icon rotate-90">
                 <ChartBarIcon className="text-[#1d9bf0] h-[22px]" />
@@ -121,16 +148,19 @@ function Input() {
               className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 
               font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] 
               disabled:opacity-50 disabled:cursor-default"
-              disabled={!input.trim() && !selectedFile /*.trim() espaço não conta como caractere*/}
-              onClick={sendPost}>
+              disabled={
+                !input.trim() &&
+                !selectedFile /*.trim() espaço não conta como caractere*/
+              }
+              onClick={sendPost}
+            >
               Tweet
             </button>
-
           </div>
         )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Input
+export default Input;
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -11,8 +11,10 @@ import {
   DotsCircleHorizontalIcon,
   DotsHorizontalIcon,
 } from "@heroicons/react/outline";
+import { useSession, signOut } from "next-auth/react";
 
 function Sidebar() {
+  const { data: session } = useSession();
   return (
     <div className="hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full">
       <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24">
@@ -28,25 +30,30 @@ function Sidebar() {
         <SidebarLink text="Profile" Icon={UserIcon} />
         <SidebarLink text="More" Icon={DotsCircleHorizontalIcon} />
       </div>
-      <button className="hidden xl:inline ml-auto bg-[#1b9bf0] 
-      text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]">
+      <button
+        className="hidden xl:inline ml-auto bg-[#1b9bf0] 
+      text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]"
+      >
         Tweet
       </button>
-      <div className="text-[#d9d9d9] flex items-center justify-center hoverAnimation 
-      xl:ml-auto xl:-mr-5 mt-auto ">
-        <img 
-          src="https://pbs.twimg.com/profile_images/1405731311914913794/c-40xdgh_400x400.jpg"
+      <div
+        className="text-[#d9d9d9] flex items-center justify-center hoverAnimation 
+      xl:ml-auto xl:-mr-5 mt-auto"
+        onClick={signOut}
+      >
+        <img
+          src={session.user.image}
           alt=""
-          className="h-10 w-10 rounded-full xl:mr-2.5" 
+          className="h-10 w-10 rounded-full xl:mr-2.5"
         />
         <div className="hidden xl:inline leading-5 ">
-          <h4 className="font-bold">Danilin</h4>
-          <p className="text-[#6e767b]">@:)))))</p>
+          <h4 className="font-bold">{session.user.name}</h4>
+          <p className="text-[#6e767b]">{session.user.tag}</p>
         </div>
         <DotsHorizontalIcon className="h-5 hidden xl:inline ml-10" />
       </div>
     </div>
-  )
+  );
 }
 
 export default Sidebar;
